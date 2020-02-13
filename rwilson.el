090ca8398a2af421397fbed02255ae7d4b259d4c
rjwilson18 <richardwilson18@gmail.com>
2020-02-12 22:58:03 -0600

    Added abbreviation handdler, babel-load-languages, and a couple packages

--- rwilson.el
+++ rwilson.el
@@ -1,3 +1,7 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                 Manage Packages                    ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Set up repos and ensure desired files are installed
 (require 'package)
 
@@ -32,7 +36,7 @@ Return a list of installed packages or nil for every skipped package."
 ;; (when (memq window-system '(mac ns x))
 ;;  (exec-path-from-shell-initialize))
 
-;; Assuming you wish to install "iedit" and "magit"
+;; Pacakges to ensure are installed
 (ensure-package-installed
  'rainbow-delimiters
  'undo-tree
@@ -42,11 +46,25 @@ Return a list of installed packages or nil for every skipped package."
  'helm
  'xkcd
  'magit
+ 'powershell
+ 'htmlize
+ 'iedit
  )
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                    Submode Hooks                   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;Define almost-universal rainbow delimiters
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'text-mode-hook 'rainbow-delimiters-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                        Python                      ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;Did not need on this machine, but needed on work vdi
+;;(setq python-shell-interpreter "Full path to python.exe")
 
 ;;Restart Python function
 (defun restart-python-console ()
@@ -67,3 +85,22 @@ Return a list of installed packages or nil for every skipped package."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (global-set-key (kbd "C-x g") 'magit-status)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;               Abbreviations Handler                ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq-default abbrev-mode t) ;;gloablly set abbreviation mode on
+(setq abbrev-file-name "c:/users/dickw/documents/emacs/abbreviations") ;;store abbreviations here
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                      Org Mode                      ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(org-babel-do-load-languages
+ 'org-bable-load-languages
+ '(
+   (python . t)
+   (shell . t)
+   (sql . t)
+   )
+ )
